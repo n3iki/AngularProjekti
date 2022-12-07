15f18d3a8bd471c5a36179a66351a263d31cf09d
n3iki / Joonas Nissinen <97595973+n3iki@users.noreply.github.com>
2022-12-07 13:19:46 +0200

    buildattu

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyBT_HrHleaiC8311o2eGxaRfb75SwFgJ-0",
+    authDomain: "lab-angular-projekti.firebaseapp.com",
+    projectId: "lab-angular-projekti",
+    storageBucket: "lab-angular-projekti.appspot.com",
+    messagingSenderId: "765498842249",
+    appId: "1:765498842249:web:ae7c42a925a48b6af97a58",
+    measurementId: "G-JK30TVLK9M"
+  }
 };
